fc1e0ed439c9ae1c95bcb4c615215f48ff2c1ea8
johanjimenez90 <arleyjimenez2290@gmail.com>
2016-12-15 22:35:36 -0500

    esta es para el nuevo repositorio

---
+++ hasta-tres/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	mimor, princesa, meses, nosamamos := "jessi", "mivida", 3, true
+	fmt.Printf("te amo %s, eres %s estoy feliz de compartir estos %d meses \n ", mimor, princesa, meses)
+	fmt.Println("te amo demasiado es ", nosamamos)
+
+}
